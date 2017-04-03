73a5679abbf3aaef70a27cbf4c4fcc91086077f1
Eduardo Patrocinio <epatro@gmail.com>
2017-04-03 15:50:00 -0400

    Set Docker API version

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
-     -v /var/lib/docker/vfs/dir:/var/lib/docker/vfs/dir patrocinio/docker-backup -addr /var/run/docker.sock $*
+     -v /var/lib/docker/vfs/dir:/var/lib/docker/vfs/dir -e DOCKER_API_VERSION=1.24 patrocinio/docker-backup -addr /var/run/docker.sock $*
